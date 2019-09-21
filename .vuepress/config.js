15ef5b767fd58affde2ef2e51a3f9d81bdf25d28
qinaichen <455905502@qq.com>
2019-09-22 00:55:40 +0800

    设计模式

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: '编程笔记',
+    port: 1337,
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar: {
+            "/DesignPatterns/": [
+                '',
+                '单一职责原则'
+            ]
+        }
+    }
+}
